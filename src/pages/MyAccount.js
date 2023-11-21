98576f2d0de9673b05895fca88ab88c69c75bda7
beion2000 <beionpoly5@gmail.com>
2023-11-21 11:45:54 -0600

    updated myacccount editing,not 100% functional yet

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { auth, db } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { reauthenticateWithCredential, EmailAuthProvider, updateEmail, sendEmailVerification } from "firebase/auth";
 
 export const MyAccount = () => {
   const [userData, setUserData] = useState(null);
@@ -16,33 +17,84 @@ export const MyAccount = () => {
           if (docSnap.exists()) {
             setUserData(docSnap.data());
           } else {
-            // Handle the case where the user document does not exist
             console.log("No such document!");
           }
         } catch (error) {
-          // Handle errors, e.g., permission denied
           console.error("Error getting document: ", error);
         }
       } else {
-        // Handle the case where no user is signed in
         console.log("No user is signed in.");
-        // You might want to redirect to the login page here
       }
     });
 
     return () => {
-      unsubscribe(); // Unsubscribe from the listener when the component unmounts
+      unsubscribe();
     };
-  }, []); // Empty dependency array ensures the effect runs once after initial render
+  }, []);
+
+  const reauthenticate = async (password) => {
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.error("No user is signed in.");
+      return;
+    }
+
+    const credentials = EmailAuthProvider.credential(user.email, password);
+    try {
+      await reauthenticateWithCredential(user, credentials);
+    } catch (error) {
+      console.error("Error reauthenticating: ", error);
+      // Handle re-authentication error here
+    }
+  };
+
+  const handleResetEmail = async () => {
+    const password = prompt("Please enter your password to reset your email:");
+
+    if (!password) {
+      console.error("Password is required.");
+      return;
+    }
+
+    await reauthenticate(password);
+
+    const newEmail = prompt("Enter your new email:");
+    if (newEmail) {
+      try {
+        const user = auth.currentUser;
+        await updateEmail(user, newEmail);
+        await sendEmailVerification(user);
+        alert("A verification email has been sent to the new email address.");
+      } catch (error) {
+        console.error("Error updating email: ", error);
+        // Handle email update error here
+      }
+    }
+  };
+
+  const handleResetPhoneNumber = async () => {
+    const password = prompt("Please enter your password to reset your phone number:");
+
+    if (!password) {
+      console.error("Password is required.");
+      return;
+    }
+
+    await reauthenticate(password);
+
+    const newPhoneNumber = prompt("Enter your new phone number:");
+    if (newPhoneNumber) {
+      // Update phone number logic here
+    }
+  };
 
   if (!userData) {
-    // Loading state or redirect to login page if no user data is available
     return <div>Loading...</div>;
   }
 
-  // Render your UI with userData
   return (
-    <div className="wrapper"> {/* Apply wrapper class for styling */}
+    <div className="wrapper">
       <h2>My Account</h2>
       <div className="input-box">
         <p>First Name: {userData.firstname}</p>
@@ -50,6 +102,8 @@ export const MyAccount = () => {
         <p>Email: {userData.email}</p>
         <p>Phone Number: {userData.phonenumber}</p>
         <p>Role: {userData.role}</p>
+        <button onClick={handleResetEmail}>Reset Email</button>
+        <button onClick={handleResetPhoneNumber}>Reset Phone Number</button>
       </div>
     </div>
   );
