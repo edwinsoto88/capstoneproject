c38f2238c37cf706f762d6eb8e84dcd32270787d
Edwin Soto <edwinsoto-villela@my.unt.edu>
2023-12-03 16:21:33 -0600

    Finished up styling for landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,32 +2,38 @@ import React from "react";
 
 export const Home = () => {
   return (
-     <div className="element-panel-intro">
-      <div className="overlap-group">
-        <div className="requestride-g">
-          <img className="carandstreet" alt="Carandstreet" src="/Images/carandstreet.png" />
-          <p className="request-a-ride-get-p">Request a ride get picked up by a nearby community driver</p>
-          <div className="text-wrapper">Request Ride</div>
+    <div className="flex-container">
+      <div className="boxes">
+        <div className="box1-container">
+            <img className="white-background-1" alt="White background" src="/Images/carandstreet.png"/>
+            <img className="city-driver-img" alt="A driver leaning on a car" src="/Images/city-driver-bro-1.png"/>
+            <div className="request-ride-heading">Request Ride</div>
+            <p className="request-ride-p">
+              Request a ride get picked up by a nearby community driver
+            </p>
         </div>
-        <img className="city-driver-bro" alt="City driver bro" src="/Images/city-driver-bro-1.png" />
+        <div className="box2-container">
+            <img className="white-background-2" alt="White background" src="/Images/redlocationicon.png"/>
+            <img className="city-skyline-img" alt="A view of a park with a city in the background" src="/Images/city-skyline-bro-1.png"/>
+            <div className="confirm-driver-heading">Confirm Your Driver</div>
+            <p className="confirm-driver-p">
+              Know your driver in advance and be able to view current location in
+              real time on the map
+            </p>
+        </div>
+        <div className="box3-container">
+            <img className="white-background-3" alt="White background" src="/Images/redlocationicon.png"/>
+            <img className="navigation-bro" alt="Guy using gps to navigate" src="/Images/navigation-bro-1.png"/>
+            <div className="track-ride-heading">Track your ride</div>
+            <p className="track-ride-p">
+              Huge drivers network helps you find comforable, safe and cheap ride
+            </p>
+        </div>
       </div>
-      <div className="overlap">
-        <div className="confirmdriver-g">
-          <img className="phoneandstreet" alt="Phoneandstreet" src="/Images/redlocationicon.png" />
-          <p className="know-your-driver-in">
-            Know your driver in advance and be able to view current location in real time on the map
-          </p>
-          <div className="div">Confirm Your Driver</div>
+      <div className="footer-section">
+        <div className="footer-section-child">
+          <b className="unt-rides">© 2023 UNT Rides</b>
         </div>
-        <img className="city-skyline-bro" alt="City skyline bro" src="/Images/city-skyline-bro-1.png" />
-      </div>
-      <div className="overlap-2">
-        <div className="trackride-g">
-          <p className="huge-drivers-network">Huge drivers network helps you find comforable, safe and cheap ride</p>
-          <div className="text-wrapper-2">Track your ride</div>
-          <img className="redlocationicon" alt="Redlocationicon" src="/Images/redlocationicon.png" />
-        </div>
-        <img className="navigation-bro" alt="Navigation bro" src="/Images/navigation-bro-1.png" />
       </div>
     </div>
   );
